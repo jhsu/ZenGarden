532a96840efa1c6a11f4f6110690d2a822cbe126
Joseph Hsu <jhsu@josephhsu.com>
2010-12-22 17:37:26 -0500

    user rvm

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,25 @@
 $:.unshift(File.expand_path("~/.rvm/lib"))
 require 'rvm/capistrano'
+require 'bundler/capistrano'
 
 set :user, "http"
 set :use_sudo, false
 
 set :rvm_ruby_string, '1.9.2-p0'
-set :rvm_type, :system
+set :rvm_type, :user
 
 set :application, "ZenGarden"
 set :scm, :git
 set :repository,  "https://jhsu@github.com/jhsu/ZenGarden.git"
+set :branch, "master"
 set :deploy_to, "/home/http/zengarden"
-
 
 role :web, "jhsu.org"                          # Your HTTP server, Apache/etc
 role :app, "jhsu.org"                          # This may be the same as your `Web` server
 role :db,  "jhsu.org", :primary => true # This is where Rails migrations will run
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
